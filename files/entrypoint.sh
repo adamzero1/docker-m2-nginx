b04121f3b0c1d9fc036ba2f1a64836dcec98e3b9
adamzero1 <adam.crossland@zero1.co.uk>
2016-12-29 22:32:35 +0000

    Update entrypoint.sh

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -2,5 +2,5 @@
 # Add user stuff
 useradd -U -m -u ${DEFAULT_USER_UID} -G sudo,www-data -d /home/magento ${DEFAULT_USER}
 sed -i 's/user www-data/user ${DEFAULT_USER}/' /etc/nginx/nginx.conf
-
+grep user /etc/nginx/nginx.conf
 nginx
